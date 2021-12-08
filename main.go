6f1e0b5a67e11016c51afcd636bfa0ab85539300
DiegoAGtz <diegogtz1207@gmail.com>
2021-12-08 15:12:16 -0600

    Reordenamiento de funciones

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-
 	_"github.com/go-sql-driver/mysql"
 )
 
@@ -23,70 +22,21 @@ func main() {
     http.HandleFunc("/borrar", Borrar)
     http.HandleFunc("/editar", Editar)
     http.HandleFunc("/actualizar", Actualizar)
-    log.Println("Servidor ejecutando...")
+    log.Println("Servidor ejecutando en el puerto 3000")
     http.ListenAndServe(":3000", nil)
 }
 
-func Actualizar(w http.ResponseWriter, r *http.Request) {
-    if r.Method == "POST" {
-        id := r.FormValue("id")
-        nombre := r.FormValue("nombre")
-        correo := r.FormValue("correo")
+func conexionDB()(conexion *sql.DB) {
+    Driver := "mysql"
+    Usuario := "laravel"
+    Contra := "laravel"
+    Nombre := "sistema_go"
 
-        conexionCreada := conexionDB()
-        modificarRegistro, err := conexionCreada.Prepare("UPDATE usuarios SET nombre = ?, correo = ? WHERE id = ?")
-        if err != nil {
-            panic(err.Error())
-        }
-
-        modificarRegistro.Exec(nombre, correo, id)
-        http.Redirect(w, r, "/", 301)
-    }
-}
-
-func Editar(w http.ResponseWriter, r *http.Request) {
-    idUsuario := r.URL.Query().Get("id")
-    conexionCreada := conexionDB()
-    registro, err := conexionCreada.Query("SELECT * FROM usuarios WHERE id = ?", idUsuario)
-    usuario := Usuario{}
-    for registro.Next() {
-        var id  int
-        var nombre, correo string
-        err = registro.Scan(&id, &nombre, &correo)
-        if err != nil {
-            panic(err.Error())
-        }
-        usuario.Id = id
-        usuario.Nombre = nombre
-        usuario.Correo = correo
-    }
-    plantillas.ExecuteTemplate(w, "editar", usuario)
-}
-
-func Borrar(w http.ResponseWriter, r *http.Request) {
-    idUsuario := r.URL.Query().Get("id")
-    conexionCreada := conexionDB()
-    borrarRegistro, err := conexionCreada.Prepare("DELETE FROM usuarios WHERE id = ?")
+    conexion, err := sql.Open(Driver, Usuario+":"+Contra+"@tcp(127.0.0.1:3306)/"+Nombre)
     if err != nil {
         panic(err.Error())
     }
-    borrarRegistro.Exec(idUsuario)
-    http.Redirect(w, r, "/", 301)
-}
-
-func Insertar(w http.ResponseWriter, r *http.Request) {
-    if r.Method == "POST" {
-        nombre := r.FormValue("nombre")
-        correo := r.FormValue("correo")
-
-        conexionCreada := conexionDB()
-        insertarRegistros,err := conexionCreada.Prepare("INSERT INTO usuarios(nombre, correo) VALUES(?, ?)")
-        if err != nil {
-            panic(err.Error())
-        }
-        insertarRegistros.Exec(nombre, correo)
-        http.Redirect(w, r, "/", 301)
-    }
+    return conexion
 }
 
 func Inicio(w http.ResponseWriter, r *http.Request) {
@@ -116,15 +66,64 @@ func Crear(w http.ResponseWriter, r *http.Request) {
     plantillas.ExecuteTemplate(w, "crear", nil)
 }
 
-func conexionDB()(conexion *sql.DB) {
-    Driver := "mysql"
-    Usuario := "laravel"
-    Contra := "laravel"
-    Nombre := "sistema_go"
+func Insertar(w http.ResponseWriter, r *http.Request) {
+    if r.Method == "POST" {
+        nombre := r.FormValue("nombre")
+        correo := r.FormValue("correo")
 
-    conexion, err := sql.Open(Driver, Usuario+":"+Contra+"@tcp(127.0.0.1:3306)/"+Nombre)
+        conexionCreada := conexionDB()
+        insertarRegistros,err := conexionCreada.Prepare("INSERT INTO usuarios(nombre, correo) VALUES(?, ?)")
+        if err != nil {
+            panic(err.Error())
+        }
+        insertarRegistros.Exec(nombre, correo)
+        http.Redirect(w, r, "/", 301)
+    }
+}
+
+func Borrar(w http.ResponseWriter, r *http.Request) {
+    idUsuario := r.URL.Query().Get("id")
+    conexionCreada := conexionDB()
+    borrarRegistro, err := conexionCreada.Prepare("DELETE FROM usuarios WHERE id = ?")
     if err != nil {
         panic(err.Error())
     }
-    return conexion
+    borrarRegistro.Exec(idUsuario)
+    http.Redirect(w, r, "/", 301)
+}
+
+func Editar(w http.ResponseWriter, r *http.Request) {
+    idUsuario := r.URL.Query().Get("id")
+    conexionCreada := conexionDB()
+    registro, err := conexionCreada.Query("SELECT * FROM usuarios WHERE id = ?", idUsuario)
+    usuario := Usuario{}
+    for registro.Next() {
+        var id  int
+        var nombre, correo string
+        err = registro.Scan(&id, &nombre, &correo)
+        if err != nil {
+            panic(err.Error())
+        }
+        usuario.Id = id
+        usuario.Nombre = nombre
+        usuario.Correo = correo
+    }
+    plantillas.ExecuteTemplate(w, "editar", usuario)
+}
+
+func Actualizar(w http.ResponseWriter, r *http.Request) {
+    if r.Method == "POST" {
+        id := r.FormValue("id")
+        nombre := r.FormValue("nombre")
+        correo := r.FormValue("correo")
+
+        conexionCreada := conexionDB()
+        modificarRegistro, err := conexionCreada.Prepare("UPDATE usuarios SET nombre = ?, correo = ? WHERE id = ?")
+        if err != nil {
+            panic(err.Error())
+        }
+
+        modificarRegistro.Exec(nombre, correo, id)
+        http.Redirect(w, r, "/", 301)
+    }
 }
